e7326abe4439301e2eb88cd3c0e311557d987042
Linyu Yang <yly1228@foxmail.com>
2025-08-10 21:06:59 +0800

    update: HOAS

--- src/HOAS/Core.hs
+++ src/HOAS/Core.hs
@@ -11,31 +11,32 @@ import           Control.Monad.Except
 import           Data.List
 import           Text.Printf
 
-data Binder a b = Bind Int ([Var a] -> b)
+data Binder a b = Bind Int Int ([Var a] -> b)
 
 data Var a = Var a | Idx Int
-  deriving Show
 
-data Bind t = A t | B (Var t)
+instance Show a => Show (Var a) where
+  show (Var a) = show a
+  show (Idx n) = printf "x%d" n
 
 type (|-) = Binder
 infixr |-
 
 instance Show b => Show (Binder a b) where
-  show (Bind nb f) = printf "%s |- %s" vars (show $ f (map Idx ids)) where
-    ids = [0 .. nb-1]
+  show (Bind start nb f) = printf "%s |- %s" vars (show $ f (map Idx ids)) where
+    ids = [start .. start + nb - 1]
     vars = intercalate "," $ map (("x" ++) . show) ids
 
 
 -- | substitution of bound variables
 subst :: (a |- b) -> [a] -> Except String b
-subst (Bind nb f) xs = do
+subst (Bind _ nb f) xs = do
   guard (nb == length xs)
   return (f $ map Var xs)
 
 -- | a language of type b should embed a variable in to it
 class Variable a b where
-  unvar :: Var a -> b
+  var :: Var a -> b
 
 
 
@@ -49,18 +50,18 @@ data Term
   deriving (Show)
 
 instance Variable Term Term where
-  unvar (Var t) = t
-  unvar (Idx i) = VarT (Idx i)
+  var (Var t) = t
+  var (Idx i) = VarT (Idx i)
 
 eg1 :: Term
-eg1 = AbsT (Bind 2 \[x, y] -> unvar x)
+eg1 = AbsT (Bind 0 2 \[x, y] -> var x)
 
 eg2 :: Term
-eg2 = AbsT (Bind 2 \[x, y] -> unvar x) @ AbsT (Bind 1 \[x] -> unvar x)
+eg2 = AbsT (Bind 0 2 \[x, y] -> var x) @ AbsT (Bind 0 1 \[x] -> var x)
 
-lam f = AbsT (Bind 1 \[x] -> f x)
+lam f = AbsT (Bind 0 1 \[x] -> f x)
 
 (@) = AppT
 
 eg3 :: Term
-eg3 = lam \x -> lam \y -> unvar x @ unvar y
+eg3 = lam \x -> lam \y -> var x @ var y
